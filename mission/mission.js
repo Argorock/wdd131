dc924b161d9bec177c4892621726c84c1c5fa9ff
Daniel Wahlquist <triforce63@icloud.com>
2025-01-18 23:20:29 -0700

    dark mode creation

--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,25 @@
-let selectElem = document.querySelector("select");
-let log = document/querySelector("img");
+const themeSelector = document.querySelector("select");
+const img = document.querySelector("img");
+
+
+function changeTheme() {
+    const currentValue = themeSelector.value;
+    console.log(currentValue);
+
+    if (currentValue === "Dark") { 
+        document.body.classList.add("dark"); 
+        console.log("Dark class added"); 
+        img.src = "byui-logo_white.png";
+    } else { 
+        document.body.classList.remove("dark"); 
+        console.log("Dark class removed"); 
+        img.src = "byui-logo_blue.webp";
+    }
+    console.log("After Condition Check");
+
+
+}
+
+// let selectElem = document.querySelector("select");
+themeSelector.addEventListener("change", changeTheme);
+
