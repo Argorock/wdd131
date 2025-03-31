3389539585b1bce09259190d796d905f81db56e4
Daniel Wahlquist <triforce63@icloud.com>
2025-03-31 10:31:04 -0600

    dice animation

--- character-sheet/character-sheet.js
+++ character-sheet/character-sheet.js
@@ -11,3 +11,37 @@ document.querySelectorAll('.dice-button').forEach(button => {
         img.src = originalSrc;
     });
 });
+
+const buttons = document.querySelectorAll('.dice-button');
+const popover = document.getElementById('dice-popover');
+const popoverContent = document.getElementById('popover-dice-content');
+const closePopoverButton = document.getElementById('close-popover');
+
+let rollingInterval;
+
+function rollDice() {
+    const diceImages = [
+        'images/dice-1/icons/roll-dice-1.png',
+        'images/dice-1/icons/roll-dice-2.png',
+        'images/dice-1/icons/roll-dice-3.png',
+        'images/dice-1/icons/roll-dice-4.png',
+        'images/dice-1/icons/roll-dice-5.png',
+        'images/dice-1/icons/roll-dice-6.png'
+    ];
+
+    const diceImage = document.querySelector('#dice-popover-img'); 
+
+    
+    rollingInterval = setInterval(() => {
+        const randomIndex = Math.floor(Math.random() * diceImages.length); 
+            diceImage.src = diceImages[randomIndex];
+    }, 100);
+
+   
+    setTimeout(() => {
+        clearInterval(rollingInterval);
+    }, 2000); 
+}
+
+
+document.querySelector('.roll').addEventListener('click', rollDice);
